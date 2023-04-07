d09cdc5c95aa881f27a97a9c837f4dd7055ec7f0
programarivm <info@programarivm.com>
2023-04-07 12:43:03 +0200

    Updated ChessServer\Command\HeuristicsCommand

--- src/Command/HeuristicsCommand.php
+++ src/Command/HeuristicsCommand.php
@@ -7,12 +7,9 @@ class HeuristicsCommand extends AbstractCommand
     public function __construct()
     {
         $this->name = '/heuristics';
-        $this->description = "Takes a balanced heuristic picture of the current game.";
+        $this->description = "Takes a balanced heuristic picture of the given PGN movetext.";
         $this->params = [
             'movetext' => '<string>',
-        ];
-        $this->dependsOn = [
-            StartCommand::class,
         ];
     }
 
