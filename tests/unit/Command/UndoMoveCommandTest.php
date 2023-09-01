065b549c7fbd620826c1b14608ba8e4b64842f26
programarivm <info@programarivm.com>
2023-09-01 13:03:04 +0200

    Fixed UndoMoveCommandTest

--- tests/unit/Command/UndoMoveCommandTest.php
+++ tests/unit/Command/UndoMoveCommandTest.php
@@ -2,7 +2,6 @@
 
 namespace ChessServer\Tests\Unit\Command;
 
-use ChessServer\Command\UndoMoveCommand;
 use ChessServer\Exception\ParserException;
 use ChessServer\Tests\Unit\CommandTestCase;
 
@@ -11,20 +10,10 @@ class UndoMoveCommandTest extends CommandTestCase
     /**
      * @test
      */
-    public function validate_undo_move()
-    {
-        $this->assertInstanceOf(
-            UndoMoveCommand::class,
-            self::$parser->validate('/undo_move')
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function validate_undo_move_foo()
+    public function validate_undo_foo()
     {
         $this->expectException(ParserException::class);
-        self::$parser->validate('/undo_move foo');
+        
+        self::$parser->validate('/undo foo');
     }
 }
