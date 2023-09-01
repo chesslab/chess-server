9efd8ceb6b153e855c2af7180fe275754e9575c4
programarivm <info@programarivm.com>
2023-09-01 13:01:50 +0200

    Fixed TakebackCommandTest

--- tests/unit/Command/TakebackCommandTest.php
+++ tests/unit/Command/TakebackCommandTest.php
@@ -2,7 +2,6 @@
 
 namespace ChessServer\Tests\Unit\Command;
 
-use ChessServer\Command\TakebackCommand;
 use ChessServer\Exception\ParserException;
 use ChessServer\Tests\Unit\CommandTestCase;
 
@@ -11,60 +10,9 @@ class TakebackCommandTest extends CommandTestCase
     /**
      * @test
      */
-    public function validate_takeback_accept()
-    {
-        $this->assertInstanceOf(
-            TakebackCommand::class,
-            self::$parser->validate('/takeback accept')
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function validate_takeback_decline()
-    {
-        $this->assertInstanceOf(
-            TakebackCommand::class,
-            self::$parser->validate('/takeback decline')
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function validate_takeback_proposes()
-    {
-        $this->assertInstanceOf(
-            TakebackCommand::class,
-            self::$parser->validate('/takeback propose')
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function validate_takeback_foo()
+    public function validate_takeback_foobar()
     {
         $this->expectException(ParserException::class);
-        self::$parser->validate('/takeback foo');
-    }
-
-    /**
-     * @test
-     */
-    public function validate_takeback_bar()
-    {
-        $this->expectException(ParserException::class);
-        self::$parser->validate('/takeback bar');
-    }
-
-    /**
-     * @test
-     */
-    public function validate_takeback_accept_foo()
-    {
-        $this->expectException(ParserException::class);
-        self::$parser->validate('/takeback accept foo');
+        self::$parser->validate('/takeback foobar');
     }
 }
