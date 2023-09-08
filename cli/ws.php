0b7ce0b8d29e2aaf827127a6960fd35944cbbf9c
programarivm <info@programarivm.com>
2023-09-08 14:45:23 +0200

    Added .env variables

--- cli/ws.php
+++ cli/ws.php
@@ -3,11 +3,15 @@
 namespace ChessServer\Cli;
 
 use ChessServer\Socket\WebSocket;
+use Dotenv\Dotenv;
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
 require __DIR__  . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
 
 $server = IoServer::factory(
     new HttpServer(
@@ -15,7 +19,7 @@ $server = IoServer::factory(
             new WebSocket()
         )
     ),
-    8080
+    $_ENV['WS_PORT']
 );
 
 $server->run();
