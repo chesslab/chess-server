3d70ca65572b004dd207b79279702cb38b33aaaf
programarivm <info@programarivm.com>
2023-09-09 22:58:34 +0200

    Fixed RestartCommand

--- src/Command/RestartCommand.php
+++ src/Command/RestartCommand.php
@@ -28,45 +28,41 @@ class RestartCommand extends AbstractCommand
 
     public function run(ChesslaBlab $socket, array $argv, int $resourceId)
     {
-        $gameMode = $socket->getGameModeStorage()->getByHash($argv[1]);
+        if ($gameMode = $socket->getGameModeStorage()->getByHash($argv[1])) {
+            $decoded = $gameMode->getJwtDecoded();
+            $decoded->iat = time();
+            $decoded->exp = time() + 3600; // one hour by default
+            if ($decoded->variant === Game::VARIANT_960) {
+                $startPos = str_split($decoded->startPos);
+                $board = (new Chess960FenStrToBoard($decoded->fen, $startPos))->create();
+                $game = (new Game($decoded->variant, Game::MODE_PLAY))->setBoard($board);
+            } else if ($decoded->variant === Game::VARIANT_CAPABLANCA) {
+                $game = new Game($decoded->variant, Game::MODE_PLAY);
+            } else {
+                $game = new Game($decoded->variant, Game::MODE_PLAY);
+            }
+            $newJwt = JWT::encode((array)$decoded, $_ENV['JWT_SECRET'], 'HS256');
+            $newGameMode = new PlayMode(
+                $game,
+                $gameMode->getResourceIds(),
+                $newJwt
+            );
+            $newGameMode->setStatus(PlayMode::STATUS_ACCEPTED)
+                ->setStartedAt(time())
+                ->setUpdatedAt(time())
+                ->setTimer([
+                    Color::W => $decoded->min * 60,
+                    Color::B => $decoded->min * 60,
+                ]);
+            $socket->getGameModeStorage()->set($newGameMode);
 
-        if (!$gameMode) {
-            throw new InternalErrorException();
-        }
-
-        $decoded = $gameMode->getJwtDecoded();
-        $decoded->iat = time();
-        $decoded->exp = time() + 3600; // one hour by default
-        if ($decoded->variant === Game::VARIANT_960) {
-            $startPos = str_split($decoded->startPos);
-            $board = (new Chess960FenStrToBoard($decoded->fen, $startPos))->create();
-            $game = (new Game($decoded->variant, Game::MODE_PLAY))->setBoard($board);
-        } else if ($decoded->variant === Game::VARIANT_CAPABLANCA) {
-            $game = new Game($decoded->variant, Game::MODE_PLAY);
-        } else {
-            $game = new Game($decoded->variant, Game::MODE_PLAY);
-        }
-        $newJwt = JWT::encode((array)$decoded, $_ENV['JWT_SECRET'], 'HS256');
-        $newGameMode = new PlayMode(
-            $game,
-            $gameMode->getResourceIds(),
-            $newJwt
-        );
-        $newGameMode->setStatus(PlayMode::STATUS_ACCEPTED)
-            ->setStartedAt(time())
-            ->setUpdatedAt(time())
-            ->setTimer([
-                Color::W => $decoded->min * 60,
-                Color::B => $decoded->min * 60,
+            return $socket->sendToMany($newGameMode->getResourceIds(), [
+                $this->name => [
+                    'jwt' => $newJwt,
+                    'hash' => md5($newJwt),
+                    'timer' => $newGameMode->getTimer(),
+                ],
             ]);
-        $socket->getGameModeStorage()->set($newGameMode);
-
-        return $socket->sendToMany($newGameMode->getResourceIds(), [
-            $this->name => [
-                'jwt' => $newJwt,
-                'hash' => md5($newJwt),
-                'timer' => $newGameMode->getTimer(),
-            ],
-        ]);
+        }
     }
 }
