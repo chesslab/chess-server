1320753e21ba18483060e79ccc53bcf14ffc9d7a
kaifkh20 <kaifkhan.biz@gmail.com>
2023-10-05 21:10:54 +0530

    Updated The LegalCommandTest file to not rewrite start_classical_fen method everytime

--- tests/functional/LegalCommandTest.php
+++ tests/functional/LegalCommandTest.php
@@ -7,6 +7,8 @@ use React\Promise\Deferred;
 use React\Socket\Connector;
 use React\Socket\ConnectionInterface;
 
+include_once __DIR__."/StartCommandTest.php";
+
 
 class LegalCommandTest extends AbstractFunctionalTestCase
 {
@@ -16,52 +18,45 @@ class LegalCommandTest extends AbstractFunctionalTestCase
 
     public static $promise;
 
+    public static $startcommandtest;
+
     protected function setUp(): void
     {
         self::$connector = new Connector();
         self::$deferred = new Deferred();
         self::$promise = self::$deferred->promise();
+        self::$startcommandtest = new StartCommandTest();
+        self::$startcommandtest->setUp();
     }
 
     /**
      * @test
+    */
+    public function start_classical_fen(){
+        self::$startcommandtest->start_classical_fen();
+    } 
+
+    /**
+     * @depends start_classical_fen
      */
 
     public function legal_e2()
     {
-
         self::$connector->connect("$this->host:$this->port")->then(function (ConnectionInterface $conn) {
             $conn->on('data', function ($data) use ($conn) {
-                self::$deferred->resolve($data);
-                $conn->close();
+            self::$deferred->resolve($data);
+            $conn->close();
             });
-            $conn->write("/start classical fen");
+            $conn->write('/legal e2');
         });
 
         $response = \React\Async\await(self::$promise->then(function (string $result): string {
             return $result;
         }));
 
-        $expected = '{"\/start":{"variant":"classical","mode":"fen","fen":"rnbqkbnr\/pppppppp\/8\/8\/8\/8\/PPPPPPPP\/RNBQKBNR w KQkq -"}}';
+        $expected = '{"\/legal":{"color":"w","id":"P","fen":{"e3":"rnbqkbnr\/pppppppp\/8\/8\/8\/4P3\/PPPP1PPP\/RNBQKBNR b KQkq -","e4":"rnbqkbnr\/pppppppp\/8\/8\/4P3\/8\/PPPP1PPP\/RNBQKBNR b KQkq e3"}}}';
 
-        if($this->assertEquals($expected,$response)){
-            self::$connector->connect("$this->host:$this->port")->then(function (ConnectionInterface $conn) {
-                $conn->on('data', function ($data) use ($conn) {
-                    self::$deferred->resolve($data);
-                    $conn->close();
-                });
-                $conn->write('/legal e2');
-            });
-
-            $response = \React\Async\await(self::$promise->then(function (string $result): string {
-                return $result;
-            }));
-
-            $expected = '{"\/legal":{"color":"w","id":"P","fen":{"e3":"rnbqkbnr\/pppppppp\/8\/8\/8\/4P3\/PPPP1PPP\/RNBQKBNR b KQkq -","e4":"rnbqkbnr\/pppppppp\/8\/8\/4P3\/8\/PPPP1PPP\/RNBQKBNR b KQkq e3"}}}';
-
-            $this->assertEquals($expected, $response);
-        }
-
+        $this->assertEquals($expected, $response);      
     }
 
 }
