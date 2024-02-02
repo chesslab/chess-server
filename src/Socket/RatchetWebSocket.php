6eed4ff5a8681139263c947303139e55bacc4050
programarivm <info@programarivm.com>
2024-02-02 17:30:34 +0100

    Cleanup

--- src/Socket/RatchetWebSocket.php
+++ src/Socket/RatchetWebSocket.php
@@ -73,42 +73,4 @@ class RatchetWebSocket extends ChesslaBlabSocket implements MessageComponentInte
 
         $this->log->info('Occurred an error', ['message' => $e->getMessage()]);
     }
-
-    public function sendToOne(int $resourceId, array $res): void
-    {
-        if (isset($this->clients[$resourceId])) {
-            $this->clients[$resourceId]->send(json_encode($res));
-
-            $this->log->info('Sent message', [
-                'id' => $resourceId,
-                'cmd' => array_keys($res),
-            ]);
-        }
-    }
-
-    public function sendToMany(array $resourceIds, array $res): void
-    {
-        foreach ($resourceIds as $resourceId) {
-            $this->clients[$resourceId]->send(json_encode($res));
-        }
-
-        $this->log->info('Sent message', [
-            'ids' => $resourceIds,
-            'cmd' => array_keys($res),
-        ]);
-    }
-
-    public function sendToAll(): void
-    {
-        $res = [
-            'broadcast' => [
-                'onlineGames' => $this->gameModeStorage
-                    ->decodeByPlayMode(PlayMode::STATUS_PENDING, PlayMode::SUBMODE_ONLINE),
-            ],
-        ];
-
-        foreach ($this->clients as $client) {
-            $client->send(json_encode($res));
-        }
-    }
 }
