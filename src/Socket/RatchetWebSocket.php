722494cc832a328e75ad008750c5dff2f42bd23c
programarivm <info@programarivm.com>
2024-02-04 14:04:55 +0100

    Cleanup

--- src/Socket/RatchetWebSocket.php
+++ src/Socket/RatchetWebSocket.php
@@ -3,7 +3,6 @@
 namespace ChessServer\Socket;
 
 use ChessServer\Command\LeaveCommand;
-use ChessServer\Game\PlayMode;
 use ChessServer\Exception\InternalErrorException;
 use ChessServer\Exception\ParserException;
 use Ratchet\MessageComponentInterface;
