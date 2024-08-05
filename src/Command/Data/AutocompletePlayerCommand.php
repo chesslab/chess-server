26fc2325726461f53102ac68e99593e846b6afa4
programarivm <info@programarivm.com>
2024-08-05 18:21:48 +0200

    Implemented AutocompletePlayerCommand

---
+++ src/Command/Data/AutocompletePlayerCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ChessServer\Command\Data;
+
+use ChessServer\Data\Pdo;
+use ChessServer\Command\AbstractCommand;
+use ChessServer\Socket\ChesslaBlabSocket;
+
+class AutocompletePlayerCommand extends AbstractCommand
+{
+    public function __construct()
+    {
+        $this->name = '/autocomplete_player';
+        $this->description = 'Autocomplete data for chess players.';
+        $this->params = [
+            'settings' => '<string>',
+        ];
+    }
+
+    public function validate(array $argv)
+    {
+        return count($argv) - 1 === count($this->params);
+    }
+
+    public function run(ChesslaBlabSocket $socket, array $argv, int $id)
+    {
+        $conf = include(__DIR__.'/../../../config/database.php');
+
+        $params = json_decode(stripslashes($argv[1]), true);
+
+        $key = key($params);
+
+        $values[] = [
+            'param' => ":$key",
+            'value' => '%'. current($params) .'%',
+            'type' => \PDO::PARAM_STR,
+        ];
+
+        $sql = "SELECT DISTINCT $key FROM games WHERE $key LIKE :$key LIMIT 10";
+
+        $arr = Pdo::getInstance($conf)
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_COLUMN);
+
+        return $socket->getClientStorage()->sendToOne($id, $arr);
+    }
+}
