e1b8e72ddfaa4c3ab1cb8151e891adeab10da51e
programarivm <info@programarivm.com>
2024-08-05 19:06:37 +0200

    Implemented SearchCommand

--- src/Command/Data/CommandContainer.php
+++ src/Command/Data/CommandContainer.php
@@ -11,6 +11,7 @@ class CommandContainer extends AbstractCommandContainer
         $this->obj = new \SplObjectStorage;
         $this->obj->attach(new AutocompleteEventCommand());
         $this->obj->attach(new AutocompletePlayerCommand());
+        $this->obj->attach(new SearchCommand());
         $this->obj->attach(new StatsEventCommand());
         $this->obj->attach(new StatsOpeningCommand());
         $this->obj->attach(new StatsPlayerCommand());
---
+++ src/Command/Data/SearchCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace ChessServer\Command\Data;
+
+use ChessServer\Data\Pdo;
+use ChessServer\Command\AbstractCommand;
+use ChessServer\Socket\ChesslaBlabSocket;
+
+class SearchCommand extends AbstractCommand
+{
+    const SQL_LIKE = [
+        'Date',
+        'movetext',
+    ];
+
+    const SQL_EQUAL = [
+        'Event',
+        'White',
+        'Black',
+        'ECO',
+        'Result',
+    ];
+
+    public function __construct()
+    {
+        $this->name = '/search';
+        $this->description = 'Finds up to 25 games matching the criteria.';
+        $this->params = [
+            'settings' => '<string>',
+        ];
+    }
+
+    public function validate(array $argv)
+    {
+        return count($argv) - 1 === count($this->params);
+    }
+
+    public function run(ChesslaBlabSocket $socket, array $argv, int $id)
+    {
+        $params = json_decode(stripslashes($argv[1]), true);
+
+        $sql = 'SELECT * FROM games WHERE ';
+
+        $values = [];
+
+        foreach ($params as $key => $val) {
+            if ($val) {
+                if (in_array($key, self::SQL_LIKE)) {
+                    $sql .= "$key LIKE :$key AND ";
+                    if ($key === 'movetext') {
+                        $val = $params['movetext'];
+                    }
+                    $values[] = [
+                        'param' => ":$key",
+                        'value' => '%'.$val.'%',
+                        'type' => \PDO::PARAM_STR,
+                    ];
+                } else if (in_array($key, self::SQL_EQUAL) && $val) {
+                    $sql .= "$key = :$key AND ";
+                    $values[] = [
+                        'param' => ":$key",
+                        'value' => $val,
+                        'type' => \PDO::PARAM_STR,
+                    ];
+                }
+            }
+        }
+
+        str_ends_with($sql, 'WHERE ')
+            ? $sql = substr($sql, 0, -6)
+            : $sql = substr($sql, 0, -4);
+
+        $sql .= 'ORDER BY RAND() LIMIT 25';
+
+        $arr = Pdo::getInstance($this->conf()['database'])
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $socket->getClientStorage()->sendToOne($id, $arr);
+    }
+}
