b603fa46ba0a1c16fbd70974224ca3f5a13d9bea
programarivm <info@programarivm.com>
2024-08-05 12:33:18 +0200

    Implemented the database service

---
+++ cli/workerman/db.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ChessServer\Cli\Workerman;
+
+use ChessServer\Socket\WorkermanClientStorage;
+use ChessServer\Socket\WorkermanWebSocket;
+use Dotenv\Dotenv;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require __DIR__  . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->load();
+
+$logger = new Logger('log');
+$logger->pushHandler(new StreamHandler(__DIR__.'/../../storage' . '/pchess.log', Logger::INFO));
+
+$clientStorage = new WorkermanClientStorage($logger);
+
+$socketName = "websocket://{$_ENV['WSS_ADDRESS']}:{$_ENV['WSS_DB_PORT']}";
+
+$context = [
+    'ssl' => [
+        'local_cert'  => __DIR__  . '/../../ssl/fullchain.pem',
+        'local_pk' => __DIR__  . '/../../ssl/privkey.pem',
+        'verify_peer' => false,
+    ],
+];
+
+$server = (new WorkermanWebSocket($socketName, $context))->init($clientStorage);
+
+$server->run();
---
+++ src/Command/Db/CommandContainer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ChessServer\Command\Db;
+
+use ChessServer\Command\Db\StatsOpeningCommand;
+
+class CommandContainer
+{
+    private $obj;
+
+    public function __construct()
+    {
+        $this->obj = new \SplObjectStorage;
+        $this->obj->attach(new StatsOpeningCommand());
+    }
+
+    public function findByName(string $name)
+    {
+        $this->obj->rewind();
+        while ($this->obj->valid()) {
+            if ($this->obj->current()->name === $name) {
+                return $this->obj->current();
+            }
+            $this->obj->next();
+        }
+
+        return null;
+    }
+
+    public function help()
+    {
+        $o = '';
+        $this->obj->rewind();
+        while ($this->obj->valid()) {
+            $o .= $this->obj->current()->name;
+            $this->obj->current()->params ? $o .= ' ' . json_encode($this->obj->current()->params) : null;
+            $o .= ' ' . $this->obj->current()->description . PHP_EOL;
+            $this->obj->next();
+        }
+
+        return $o;
+    }
+}
