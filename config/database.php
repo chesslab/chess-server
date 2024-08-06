5dc910725c55a00d1d74b0cecf82af732f949356
Joan Miquel <joanhey@kumbiaphp.com>
2024-08-07 01:18:46 +0200

    Update database.php
    Come with another change

--- config/database.php
+++ config/database.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
-    'driver' => $_ENV['DB_DRIVER'],
-    'host' => $_ENV['DB_HOST'],
-    'database' => $_ENV['DB_DATABASE'],
-    'username' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD'],
+    'database' => [
+        'driver' => $_ENV['DB_DRIVER'],
+        'host' => $_ENV['DB_HOST'],
+        'database' => $_ENV['DB_DATABASE'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ],
 ];
