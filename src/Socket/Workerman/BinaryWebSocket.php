26c45727668c88cb2b2ec4b8c6f11abceff11569
programarivm <info@programarivm.com>
2024-08-21 11:05:24 +0200

    Set the flag for the transmission data type

--- src/Socket/Workerman/BinaryWebSocket.php
+++ src/Socket/Workerman/BinaryWebSocket.php
@@ -3,12 +3,17 @@
 namespace ChessServer\Socket\Workerman;
 
 use ChessServer\Command\Parser;
+use Workerman\Protocols\Websocket;
 
 class BinaryWebSocket extends AbstractWebSocket
 {
     public function __construct(string $socketName, array $context, Parser $parser)
     {
         parent::__construct($socketName, $context, $parser);
+
+        $this->worker->onConnect = function($conn) {
+            $conn->websocketType = Websocket::BINARY_TYPE_ARRAYBUFFER;
+        };
 
         $this->connect()->message()->error()->close();
     }
