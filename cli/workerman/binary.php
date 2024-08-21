5c03aa93ca180a44e771dadc63aaaa6d5419e0e7
programarivm <info@programarivm.com>
2024-08-21 10:56:24 +0200

    Added ChessServer\Socket\Workerman\BinaryWebSocket

--- cli/workerman/binary.php
+++ cli/workerman/binary.php
@@ -3,7 +3,9 @@
 namespace ChessServer\Cli\Workerman;
 
 use ChessServer\Command\Parser;
+use ChessServer\Command\Binary\Cli;
 use ChessServer\Socket\Workerman\ClientStorage;
+use ChessServer\Socket\Workerman\BinaryWebSocket;
 use Dotenv\Dotenv;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -13,7 +15,23 @@ require __DIR__  . '/../../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__.'/../../');
 $dotenv->load();
 
-$logger = new Logger('data');
+$logger = new Logger('binary');
 $logger->pushHandler(new StreamHandler(__DIR__.'/../../storage' . '/binary.log', Logger::INFO));
 
-echo 'TODO' . PHP_EOL;
+$parser = new Parser(new Cli());
+
+$clientStorage = new ClientStorage($logger);
+
+$socketName = "websocket://{$_ENV['WSS_ADDRESS']}:{$_ENV['WSS_BINARY_PORT']}";
+
+$context = [
+    'ssl' => [
+        'local_cert'  => __DIR__  . '/../../ssl/fullchain.pem',
+        'local_pk' => __DIR__  . '/../../ssl/privkey.pem',
+        'verify_peer' => false,
+    ],
+];
+
+$webSocket = (new BinaryWebSocket($socketName, $context, $parser))->init($clientStorage);
+
+$webSocket->getWorker()->runAll();
