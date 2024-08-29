6a3398f5b31755be70de0588bdfcfc9454f712c6
programarivm <info@programarivm.com>
2024-08-29 12:41:39 +0200

    Refactored ResultCommand

--- src/Command/Data/ResultCommand.php
+++ src/Command/Data/ResultCommand.php
@@ -6,6 +6,8 @@ use ChessServer\Socket\AbstractSocket;
 
 class ResultCommand extends AbstractDataCommand
 {
+    const RESULT_FILE = 'result.json';
+
     public function __construct(Db $db)
     {
         parent::__construct($db);
@@ -21,41 +23,9 @@ class ResultCommand extends AbstractDataCommand
 
     public function run(AbstractSocket $socket, array $argv, int $id)
     {
-        $sql = "SELECT ECO, COUNT(*) AS total
-          FROM games
-          WHERE Result = '1/2-1/2'
-          GROUP BY ECO
-          HAVING total >= 100
-          ORDER BY total DESC
-          LIMIT 50";
+        $contents = file_get_contents(AbstractSocket::DATA_FOLDER.'/'.self::RESULT_FILE);
 
-        $drawRate = $this->db->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
-
-        $sql = "SELECT ECO, COUNT(*) AS total
-          FROM games
-          WHERE Result = '1-0'
-          GROUP BY ECO
-          HAVING total >= 100
-          ORDER BY total DESC
-          LIMIT 50";
-
-        $winRateForWhite = $this->db->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
-
-        $sql = "SELECT ECO, COUNT(*) AS total
-          FROM games
-          WHERE Result = '0-1'
-          GROUP BY ECO
-          HAVING total >= 100
-          ORDER BY total DESC
-          LIMIT 50";
-
-        $winRateForBlack = $this->db->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
-
-        $arr = [
-            'drawRate' => $drawRate,
-            'winRateForWhite' => $winRateForWhite,
-            'winRateForBlack' => $winRateForBlack,
-        ];
+        $arr = json_decode($contents);
 
         return $socket->getClientStorage()->sendToOne($id, [
             $this->name => $arr,
