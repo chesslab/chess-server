fef06dd81d01143ebd80c7b0a11a70118efc31a2
programarivm <info@programarivm.com>
2024-09-04 16:57:52 +0200

    Returned the Anonymous username

--- src/Command/Game/StartCommand.php
+++ src/Command/Game/StartCommand.php
@@ -168,6 +168,7 @@ class StartCommand extends AbstractCommand
                     'iat' => time(),
                     'exp' => time() + 3600, // one hour by default
                     'variant' => $argv[1],
+                    'username' => $settings->username ?? 'Anonymous',
                     'submode' => $settings->submode,
                     'color' => $settings->color,
                     'min' => $settings->min,
