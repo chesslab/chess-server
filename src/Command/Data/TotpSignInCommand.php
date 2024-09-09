f3d869601b25adc40763ba4e9693d5104f123d39
programarivm <info@programarivm.com>
2024-09-09 09:41:12 +0200

    Renamed token as ui

--- src/Command/Data/TotpSignInCommand.php
+++ src/Command/Data/TotpSignInCommand.php
@@ -59,7 +59,7 @@ class TotpSignInCommand extends AbstractDataCommand
 
             return $socket->getClientStorage()->sendToOne($id, [
                 $this->name => [
-                    'token' => JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256'),
+                    'ui' => JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256'),
                 ],
             ]);
         }
