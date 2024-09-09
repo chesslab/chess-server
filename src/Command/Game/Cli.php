eafe137cbe6713c4a0201b00b074f786b28d0927
programarivm <info@programarivm.com>
2024-09-09 19:04:24 +0200

    Reordered the game commands

--- src/Command/Game/Cli.php
+++ src/Command/Game/Cli.php
@@ -9,24 +9,24 @@ class Cli extends AbstractCli
     public function __construct()
     {
         parent::__construct();
-        
-        $this->commands->attach(new AcceptPlayRequestCommand());
+
         $this->commands->attach(new DrawCommand());
-        $this->commands->attach(new EvalNamesCommand());
-        $this->commands->attach(new HeuristicCommand());
         $this->commands->attach(new LeaveCommand());
+        $this->commands->attach(new RematchCommand());
+        $this->commands->attach(new ResignCommand());
+        $this->commands->attach(new TakebackCommand());
+        $this->commands->attach(new AcceptPlayRequestCommand());
+        $this->commands->attach(new HeuristicCommand());
         $this->commands->attach(new LegalCommand());
-        $this->commands->attach(new OnlineGamesCommand());
         $this->commands->attach(new PlayLanCommand());
         $this->commands->attach(new PlayRavCommand());
         $this->commands->attach(new RandomizerCommand());
-        $this->commands->attach(new RematchCommand());
-        $this->commands->attach(new ResignCommand());
         $this->commands->attach(new RestartCommand());
         $this->commands->attach(new StartCommand());
         $this->commands->attach(new StockfishCommand());
-        $this->commands->attach(new TakebackCommand());
         $this->commands->attach(new TutorFenCommand());
+        $this->commands->attach(new EvalNamesCommand());
+        $this->commands->attach(new OnlineGamesCommand());
         $this->commands->attach(new UndoCommand());
     }
 }
