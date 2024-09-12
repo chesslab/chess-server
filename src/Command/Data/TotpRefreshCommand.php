b5c88516e44df8159b7e5c5c23c6b39708592c03
programarivm <info@programarivm.com>
2024-09-12 17:37:50 +0200

    Return null if no access token is provided

--- src/Command/Data/TotpRefreshCommand.php
+++ src/Command/Data/TotpRefreshCommand.php
@@ -30,28 +30,31 @@ class TotpRefreshCommand extends AbstractCommand
     {
         $params = json_decode(stripslashes($argv[1]), true);
 
-        $decoded = JWT::decode($params['access_token'], new Key($_ENV['JWT_SECRET'], 'HS256'));
-
-        $sql = "SELECT * FROM users WHERE username = :username";
-        $values[] = [
-            'param' => ":username",
-            'value' => $decoded->username,
-            'type' => \PDO::PARAM_STR,
-        ];
-        $arr = $this->db->query($sql, $values)->fetch(\PDO::FETCH_ASSOC);
-
-        $payload = [
-            'iss' => $_ENV['JWT_ISS'],
-            'iat' => time(),
-            'exp' => time() + 3600, // one hour by default
-            'username' => $arr['username'],
-            'elo' => $arr['elo'],
-        ];
+        if ($params['access_token']) {
+            $decoded = JWT::decode($params['access_token'], new Key($_ENV['JWT_SECRET'], 'HS256'));
+            $sql = "SELECT * FROM users WHERE username = :username";
+            $values[] = [
+              'param' => ":username",
+              'value' => $decoded->username,
+              'type' => \PDO::PARAM_STR,
+            ];
+            $arr = $this->db->query($sql, $values)->fetch(\PDO::FETCH_ASSOC);
+            $payload = [
+              'iss' => $_ENV['JWT_ISS'],
+              'iat' => time(),
+              'exp' => time() + 3600, // one hour by default
+              'username' => $arr['username'],
+              'elo' => $arr['elo'],
+            ];
+            return $socket->getClientStorage()->sendToOne($id, [
+              $this->name => [
+                  'access_token' => JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256'),
+              ],
+            ]);
+        }
 
         return $socket->getClientStorage()->sendToOne($id, [
-            $this->name => [
-                'access_token' => JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256'),
-            ],
+            $this->name => null,
         ]);
     }
 }
