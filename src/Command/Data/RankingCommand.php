f432ab2837bf8058306cf18f1d639b74a7585bc6
programarivm <info@programarivm.com>
2024-09-17 16:45:21 +0200

    Return the top 20 players

--- src/Command/Data/RankingCommand.php
+++ src/Command/Data/RankingCommand.php
@@ -23,7 +23,7 @@ class RankingCommand extends AbstractCommand
 
     public function run(AbstractSocket $socket, array $argv, int $id)
     {
-        $sql = "SELECT username, elo FROM users WHERE lastLoginAt IS NOT NULL ORDER BY elo DESC LIMIT 100";
+        $sql = "SELECT username, elo FROM users WHERE lastLoginAt IS NOT NULL ORDER BY elo DESC LIMIT 20";
 
         $arr = $this->db->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
 
