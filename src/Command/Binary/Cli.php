c73c5b04150ff315e441dc370295d7b88b34dfbf
programarivm <info@programarivm.com>
2024-10-07 22:59:03 +0200

    Implemented ImageAsyncTask

--- src/Command/Binary/Cli.php
+++ src/Command/Binary/Cli.php
@@ -3,13 +3,14 @@
 namespace ChessServer\Command\Binary;
 
 use ChessServer\Command\AbstractCli;
+use Spatie\Async\Pool;
 
 class Cli extends AbstractCli
 {
-    public function __construct()
+    public function __construct(Pool $pool)
     {
         parent::__construct();
 
-        $this->commands->attach(new ImageCommand());
+        $this->commands->attach((new ImageCommand())->setPool($pool));
     }
 }
--- src/Command/Binary/ImageCommand.php
+++ src/Command/Binary/ImageCommand.php
@@ -2,9 +2,6 @@
 
 namespace ChessServer\Command\Binary;
 
-use Chess\Media\BoardToPng;
-use Chess\Variant\Classical\FEN\StrToBoard as ClassicalStrToBoard;
-use Chess\Variant\Classical\PGN\AN\Color;
 use ChessServer\Command\AbstractCommand;
 use ChessServer\Socket\AbstractSocket;
 
@@ -28,17 +25,11 @@ class ImageCommand extends AbstractCommand
     {
         $params = json_decode(stripslashes($argv[1]), true);
 
-        $board = (new ClassicalStrToBoard($params['fen']))->create();
-        $filename = (new BoardToPng($board, $params['flip'] === Color::B))
-          ->output(AbstractSocket::TMP_FOLDER);
-        $contents = file_get_contents(AbstractSocket::TMP_FOLDER . "/$filename");
-        $base64 = base64_encode($contents);
-        if (is_file(AbstractSocket::TMP_FOLDER . "/$filename")) {
-            unlink(AbstractSocket::TMP_FOLDER . "/$filename");
-        }
-
-        return $socket->getClientStorage()->send([$id], [
-            $this->name => $base64,
-        ]);
+        $this->pool->add(new ImageAsyncTask($params), 81920)
+            ->then(function ($result) use ($socket, $id) {
+                return $socket->getClientStorage()->send([$id], [
+                    $this->name => $result,
+                ]);
+            });
     }
 }
