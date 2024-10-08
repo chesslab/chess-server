094fc61795872c7f8c3bb7cd57452e94cc28d7b4
programarivm <info@programarivm.com>
2024-10-08 20:47:29 +0200

    Refactored SearchCommand

--- src/Command/Data/SearchCommand.php
+++ src/Command/Data/SearchCommand.php
@@ -25,17 +25,7 @@ class SearchCommand extends AbstractCommand
     {
         $params = json_decode(stripslashes($argv[1]), true);
 
-        $env = [
-            'db' => [
-                'driver' => $_ENV['DB_DRIVER'],
-                'host' => $_ENV['DB_HOST'],
-                'database' => $_ENV['DB_DATABASE'],
-                'username' => $_ENV['DB_USERNAME'],
-                'password' => $_ENV['DB_PASSWORD'],
-            ],
-        ];
-
-        $this->pool->add(new SearchAsyncTask($params, $env), 81920)
+        $this->pool->add(new SearchAsyncTask($params), 81920)
             ->then(function ($result) use ($socket, $id) {
                 return $socket->getClientStorage()->send([$id], [
                     $this->name => $result,
