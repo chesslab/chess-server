b15f6ae3e7e76907e43e1f47bdd2b223d5d287f7
programarivm <info@programarivm.com>
2024-10-08 20:01:30 +0200

    Refactored RankingAsyncTask

--- src/Command/Data/RankingAsyncTask.php
+++ src/Command/Data/RankingAsyncTask.php
@@ -2,25 +2,8 @@
 
 namespace ChessServer\Command\Data;
 
-use ChessServer\Db;
-use Spatie\Async\Task;
-
-class RankingAsyncTask extends Task
+class RankingAsyncTask extends AbstractDataAsyncTask
 {
-    private array $env;
-
-    private Db $db;
-
-    public function __construct(array $env)
-    {
-        $this->env = $env;
-    }
-
-    public function configure()
-    {
-        $this->db = new Db($this->env['db']);
-    }
-
     public function run()
     {
         $sql = "SELECT username, elo FROM users WHERE lastLoginAt IS NOT NULL ORDER BY elo DESC LIMIT 20";
