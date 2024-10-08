281673dc0ebcfc0fcea79ebb28d124399a43ab5f
programarivm <info@programarivm.com>
2024-10-08 20:03:19 +0200

    Refactored RankingCommand

--- src/Command/Data/RankingCommand.php
+++ src/Command/Data/RankingCommand.php
@@ -20,17 +20,7 @@ class RankingCommand extends AbstractCommand
 
     public function run(AbstractSocket $socket, array $argv, int $id)
     {
-        $env = [
-            'db' => [
-                'driver' => $_ENV['DB_DRIVER'],
-                'host' => $_ENV['DB_HOST'],
-                'database' => $_ENV['DB_DATABASE'],
-                'username' => $_ENV['DB_USERNAME'],
-                'password' => $_ENV['DB_PASSWORD'],
-            ],
-        ];
-
-        $this->pool->add(new RankingAsyncTask($env))
+        $this->pool->add(new RankingAsyncTask())
             ->then(function ($result) use ($socket, $id) {
                 return $socket->getClientStorage()->send([$id], [
                     $this->name => $result,
