261eba0cc3f96968aeaf56c149ea3d679dab5f36
programarivm <info@programarivm.com>
2024-10-12 18:03:21 +0200

    Updated RestartCommand

--- src/Command/Game/Async/RestartCommand.php
+++ src/Command/Game/Async/RestartCommand.php
@@ -27,34 +27,33 @@ class RestartCommand extends AbstractAsyncCommand
     public function run(AbstractSocket $socket, array $argv, int $id)
     {
         $params = json_decode(stripslashes($argv[1]), true);
+        $gameMode = $socket->getGameModeStorage()->getByJwt($params['jwt']);
 
-        if ($gameMode = $socket->getGameModeStorage()->getByJwt($params['jwt'])) {
-            $this->pool->add(new RestartTask([
-                'decoded' => $gameMode->getJwtDecoded(),
-            ]))->then(function ($result) use ($socket, $gameMode) {
-                if ($result->variant === Game::VARIANT_960) {
-                    $startPos = str_split($result->startPos);
-                    $board = (new Chess960FenStrToBoard($result->fen, $startPos))->create();
-                    $game = (new Game($result->variant, Game::MODE_PLAY))->setBoard($board);
-                } else {
-                    $game = new Game($result->variant, Game::MODE_PLAY);
-                }
-                $gameMode->setGame($game)
-                    ->setJwt((array) $result)
-                    ->setStartedAt(time())
-                    ->setUpdatedAt(time())
-                    ->setTimer([
-                        Color::W => $result->min * 60,
-                        Color::B => $result->min * 60,
-                    ]);
-                $socket->getGameModeStorage()->set($gameMode);
-                return $socket->getClientStorage()->send($gameMode->getResourceIds(), [
-                    $this->name => [
-                        'jwt' => $gameMode->getJwt(),
-                        'timer' => $gameMode->getTimer(),
-                    ],
+        $this->pool->add(new RestartTask([
+            'decoded' => $gameMode->getJwtDecoded(),
+        ]))->then(function ($result) use ($socket, $gameMode) {
+            if ($result->variant === Game::VARIANT_960) {
+                $startPos = str_split($result->startPos);
+                $board = (new Chess960FenStrToBoard($result->fen, $startPos))->create();
+                $game = (new Game($result->variant, Game::MODE_PLAY))->setBoard($board);
+            } else {
+                $game = new Game($result->variant, Game::MODE_PLAY);
+            }
+            $gameMode->setGame($game)
+                ->setJwt((array) $result)
+                ->setStartedAt(time())
+                ->setUpdatedAt(time())
+                ->setTimer([
+                    Color::W => $result->min * 60,
+                    Color::B => $result->min * 60,
                 ]);
-            });
-        }
+            $socket->getGameModeStorage()->set($gameMode);
+            return $socket->getClientStorage()->send($gameMode->getResourceIds(), [
+                $this->name => [
+                    'jwt' => $gameMode->getJwt(),
+                    'timer' => $gameMode->getTimer(),
+                ],
+            ]);
+        });
     }
 }
