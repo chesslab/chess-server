3247bcd14c4fde1c395c42afdcfc96b1db1b800e
standard <info@programarivm.com>
2025-01-12 12:11:34 +0100

    Updated AbstractSyncCommand

--- src/Command/AbstractSyncCommand.php
+++ src/Command/AbstractSyncCommand.php
@@ -8,7 +8,7 @@ abstract class AbstractSyncCommand extends AbstractCommand
 {
     protected Pool $pool;
 
-    public function setPool(Pool $pool): AbstractAsyncCommand
+    public function setPool(Pool $pool): AbstractSyncCommand
     {
         $this->pool = $pool;
 
