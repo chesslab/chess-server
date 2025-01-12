df0e157f06559fb4a966e72d1abe34df441ea33d
standard <info@programarivm.com>
2025-01-12 11:23:53 +0100

    Updated AbstractSyncCommand

--- src/Command/AbstractSyncCommand.php
+++ src/Command/AbstractSyncCommand.php
@@ -2,6 +2,16 @@
 
 namespace ChessServer\Command;
 
+use Spatie\Async\Pool;
+
 abstract class AbstractSyncCommand extends AbstractCommand
 {
+    protected Pool $pool;
+
+    public function setPool(Pool $pool): AbstractAsyncCommand
+    {
+        $this->pool = $pool;
+
+        return $this;
+    }
 }
